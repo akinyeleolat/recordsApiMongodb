7a5da5e4adc9ed59ca194392b4b43f04ee3db13a
Tosin Akinyele <tosinakinyele@Tosins-MacBook-Pro.local>
2020-12-20 11:11:59 +0100

    remove unnecessary comment

--- src/spec/controllers/record.spec.js
+++ src/spec/controllers/record.spec.js
@@ -7,34 +7,6 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Records Controller', () => {
-//   it('should return 404 for a user that does not exist', (done) => {
-//     chai
-//       .request(app)
-//       .post('/auth/login')
-//       .send({ email: 'ututu@rr.com', password: 'tetete123' })
-//       .end((err, res) => {
-//         expect(res.status).to.be.equal(404);
-//         expect(res.body)
-//           .to.have.property('message')
-//           .equal('User does not exist');
-//         done();
-//       });
-//   });
-
-  //   it('should not allow wrong user credentials', (done) => {
-  //     chai
-  //       .request(app)
-  //       .post('/auth/login')
-  //       .send({ email: goodUserData.email, password: 'tetete123' })
-  //       .end((err, res) => {
-  //         expect(res.status).to.be.equal(400);
-  //         expect(res.body)
-  //           .to.have.property('message')
-  //           .equal('User credentials are invalid');
-  //         done();
-  //       });
-  //   });
-
   it('should fetch records', (done) => {
     chai
       .request(app)
